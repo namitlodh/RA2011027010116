b76943c46517fe21d370b497ec34ccd2f18c698a
Namit Lodh <77287454+namitlodh@users.noreply.github.com>
2023-08-22 20:07:14 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const data = {number: [1,2,3,5,8,13]}
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'application\json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(8008);
